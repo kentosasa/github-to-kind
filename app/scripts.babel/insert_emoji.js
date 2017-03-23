10ab4785d1d65c610d08ccb129c57709b63dda3c
kento sasamoto <knote0721@gmail.com>
2017-03-23 18:32:48 +0900

    insert emoji

---
+++ app/scripts.babel/insert_emoji.js
@@ -0,0 +1,16 @@
+window.addEventListener ('load', main, false);
+
+function main() {
+  console.log('load github pages')
+
+  var elements = $('.review-comment-contents .comment-body').each(function() {
+    var html = $(this).html()
+    html = html
+      .replace(/書けます。*/g,'書けるよ🙄')
+      .replace(/修正しました。*/g,'修正しました🙇')
+      .replace(/なりそうです。*/g,'なりそう🤔')
+      .replace(/思います。*/g,'思う🤔')
+
+    $(this).html(html)
+  })
+}
